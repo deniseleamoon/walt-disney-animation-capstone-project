1d8ad8d2736cadec69df84912b22e32f509a4283
deniseleamoon <deniseleamoon@gmail.com>
2023-11-30 02:08:13 -0500

    added a drop-down menu to top of page and social media links to the footer of the Wish movie page

--- wish-page.js
+++ wish-page.js
@@ -68,4 +68,12 @@
 
   })
 
+  //drop down menu
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "50%";
+  }
   
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
