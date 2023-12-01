03c676b792685c18445fc46f234a65dbd412036f
deniseleamoon <deniseleamoon@gmail.com>
2023-11-30 19:19:26 -0500

    made the events page responsive and cleaned up folders

--- Events-Page/wish-page.js
+++ Events-Page/wish-page.js
@@ -75,3 +75,41 @@
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+  //Modal and Video API
+
+  // Get the modal element
+  var modal = document.getElementById('videoModal');
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("openModal");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+    var video = document.getElementById("youtubeVideo");
+    video.src = video.src + "&autoplay=1";
+  }
+  
+  // When the user clicks on <span> (x), close the modal and stop the video
+  span.onclick = function() {
+    modal.style.display = "none";
+    var video = document.getElementById("youtubeVideo");
+    video.src = video.src.replace("&autoplay=1", "");
+    video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it and stop the video
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      var video = document.getElementById("youtubeVideo");
+      video.src = video.src.replace("&autoplay=1", "");
+      video.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+    }
+  }
+  
+  
